285f9440be01f7ded27ced13d0b133ebaf9b2758
Bertrand Boustany <bertrand@may.fr>
2023-09-27 22:56:12 +0200

    try without future object

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,14 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   // serverBuildTarget: "vercel",
-  future: {
+  // future: {
     // v2_dev: true,
     // v2_errorBoundary: true,
     // v2_headers: true,
     // v2_meta: true,
     // v2_normalizeFormMethod: true,
     // v2_routeConvention: false,
-  },
+  // },
   ignoredRouteFiles: ["**/.*"],
   // serverModuleFormat: "cjs",
   // appDirectory: "app",
