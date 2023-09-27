c3ae75f60f8758687c138ca39eec2b44ddb24492
Bertrand Boustany <bertrand@may.fr>
2023-09-27 16:29:02 +0200

    make the load time super slow

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@vercel/remix";
-
+import type { LoaderArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -7,10 +9,19 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export async function loader({ request }: LoaderArgs) {
+  await wait(1000 * 7); // 7 seconds
+  return json({ hello: "world" });
+}
+
 export default function Index() {
+  const { hello } = useLoaderData();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Hello { hello }!</h1>
       <ul>
         <li>
           <a
